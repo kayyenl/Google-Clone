05dc3f4214ba46dcf33ccef9751862b926232d5d
kayyenl <yenyenlee9@gmail.com>
2022-08-06 17:30:40 +0800

    experimenting with react-router now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
       <h1>hey lets build the google clone! 🚀 </h1>
+
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <div className="home__header">
+                <div className="home__header--left">
+                    {/* link */}
+                    {/* link */}
+                </div>
+                <div className="home__header--right">
+                    {/* link */}
+                    {/* link */}
+                    {/* icon */}
+                </div>
+            </div>
+            <div className="home__body"></div>
+        </div>
+    );
+}
+
+export default Home;
