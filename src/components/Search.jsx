3bc0f0b20f0bb33c7eea2c217828ef0ddb546a84
kayyenl <yenyenlee9@gmail.com>
2022-08-07 04:31:22 +0800

    working on the buttons, will continue in the morning

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import MicIcon from '@mui/icons-material/Mic';
+import Button from '@mui/material/Button';
 
 const Search = () => {
     return (
@@ -9,6 +10,11 @@ const Search = () => {
                 <SearchIcon className="search__input--icon" />
                 <input />
                 <MicIcon />
+            </div>
+
+            <div className="search__buttons">
+                <Button variant='outlined'>Google Search</Button>
+                <Button variant='outlined'>I'm feeling lucky</Button>
             </div>
         </div>
     );
