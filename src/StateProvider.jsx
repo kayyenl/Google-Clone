ba3eb8c8fcbe1db1639d22b847813fdeaad8de7b
kayyenl <yenyenlee9@gmail.com>
2022-08-08 18:00:56 +0800

    many things to learn about reducer, will come back and continue

--- src/StateProvider.jsx
+++ src/StateProvider.jsx
@@ -16,3 +16,27 @@ export const StateProvider = ({ reducer, initialState, children }) => {
 
 //hook that pulls information from the data layer
 export default useStateValue = () => useContext(StateContext)
+
+export const initialState = {
+    term: null,
+}
+
+export const actionTypes = {
+    SET_SEARCH_TERM: "SET_SEARCH_TERM",
+}
+
+const reducer = (state, action) => {
+    console.log(action)
+
+    switch (action.type) {
+        case actionTypes.SET_SEARCH_TERM:
+            return {
+                ...state,
+                term: action.term,
+            }
+        
+        default: 
+            return state
+    }
+
+}
