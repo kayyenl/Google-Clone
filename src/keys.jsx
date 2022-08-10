2def03a76b473e90c5d255938bddd806e10aa339
kayyenl <yenyenlee9@gmail.com>
2022-08-10 16:41:20 +0800

    going to make a custom hook, useGoogleSearch

--- src/keys.jsx
+++ src/keys.jsx
@@ -1,2 +1,8 @@
 export const API_KEY = "AIzaSyAMci_89_im4vE-E_AwQtOtuX45qXURbTo"
 export default API_KEY
+
+{/* <script async src="https://cse.google.com/cse.js?cx=67d2ea1c791314d6c">
+</script>
+<div class="gcse-search"></div> */}
+
+// 67d2ea1c791314d6c
---
+++ src/useGoogleSearch.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import API_KEY from './keys';
+
+const CONTEXT_KEY = '67d2ea1c791314d6c'
+// the context key above tells google which search engine to be using.
+// the one we created above searches the entire web.
+
+const UseGoogleSearch = () => {
+    
+}
+
+export default UseGoogleSearch;
