f04faf1aab055f885c2bf71a0da900484e392784
kayyenl <yenyenlee9@gmail.com>
2022-08-10 21:14:52 +0800

    starting to work on the search results icons below bar

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useStateValue } from '../StateProvider';
 import UseGoogleSearch from '../useGoogleSearch';
 import Response from '../Response'
+import { Link } from 'react-router-dom';
+import Search from '../components/Search';
 
 const SearchPage = () => {
     const [state, dispatch] = useStateValue() // can be destructured
@@ -9,12 +11,23 @@ const SearchPage = () => {
 
     const data = Response
     console.log(data)
+
+
     return (
         <div className='searchpage'>
             <div className="searchpage__header">
-                {state.term}
+                <Link to="/">
+                    <img className='searchpage__logo'
+                    src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" 
+                    alt="" />
+                </Link>
+                <div className="searchpage__header--body">
+                    <Search hideButtons />
+                </div>
             </div> 
-            <div className="searchpage__results"></div>
+            <div className="searchpage__results">
+                {state.term}
+            </div>
         </div>
     );
 }
