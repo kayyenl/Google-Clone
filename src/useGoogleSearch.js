9bf4762e420efdc586f836be6097a9371f9ad3ce
kayyenl <yenyenlee9@gmail.com>
2022-08-10 17:06:43 +0800

    going to use useeffect to call googles custom search API 2:15:29

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -5,8 +5,17 @@ const CONTEXT_KEY = '67d2ea1c791314d6c'
 // the context key above tells google which search engine to be using.
 // the one we created above searches the entire web.
 
-const UseGoogleSearch = () => {
-    
+
+// for the bottom custom hook, it will be called when the 
+// term variable is being pushed into the data layer. Then
+// due to the change in term, being one of the dependencies of 
+// useeffect, it will trigger the function written inside of useeffect.
+const UseGoogleSearch = (term) => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+
+    }, [term])
 }
 
 export default UseGoogleSearch;
