798809600e9182560029bd510b2a9ba3199df9e9
kayyenl <yenyenlee9@gmail.com>
2022-08-10 20:56:14 +0800

    just got back on track

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useStateValue } from '../StateProvider';
 import UseGoogleSearch from '../useGoogleSearch';
+import Response from '../Response'
 
 const SearchPage = () => {
     const [state, dispatch] = useStateValue() // can be destructured
-    const { data } = UseGoogleSearch(state.term)
+    // const { data } = UseGoogleSearch(state.term)
 
+    const data = Response
     console.log(data)
     return (
         <div className='searchpage'>
             <div className="searchpage__header">
-            </div>
+                {state.term}
+            </div> 
             <div className="searchpage__results"></div>
         </div>
     );
